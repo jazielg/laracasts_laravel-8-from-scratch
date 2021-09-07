7c04cd2a379907bbc5b5b38042999a2e804bb322
jazielg <jazielgj1995@gmail.com>
2021-09-07 15:01:56 -0300

    12 - find a composer package for post metadata

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,27 +4,82 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
+    public $title;
+
+    public $excerpt;
+
+    public $date;
+
+    public $body;
+
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
     public static function all()
     {
-        $files = File::files(resource_path("posts/"));
+        $files = File::files(resource_path("posts"));
+        $posts = [];
 
-        return array_map(function ($file) {
-            return $file->getContents();
-        }, $files);
+        return collect($files)->map(function ($file) {
+            return YamlFrontMatter::parseFile($file);
+        })->map(function ($document) {
+            return new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug,
+            );
+        });
+
+        // return array_map(function ($file) {
+        //     $document = YamlFrontMatter::parseFile($file);
+        //     return new Post(
+        //         $document->title,
+        //         $document->excerpt,
+        //         $document->date,
+        //         $document->body(),
+        //         $document->slug,
+        //     );
+        // }, $files);
+
+        // foreach ($files as $file) {
+        //     $document = YamlFrontMatter::parseFile($file);
+        //     $posts[] = new Post(
+        //         $document->title,
+        //         $document->excerpt,
+        //         $document->date,
+        //         $document->body(),
+        //         $document->slug,
+        //     );
+        // }
+        // return $posts;
     }
 
     public static function find($slug)
     {
-        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-            // abort(404);
-            throw new ModelNotFoundException();
-        }
+        // if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
+        //     // abort(404);
+        //     throw new ModelNotFoundException();
+        // }
 
-        return cache()->remember("posts.{$slug}", now()->addMinutes(20), function () use ($path) {
-            return file_get_contents($path);
-        });
+        // return cache()->remember("posts.{$slug}", now()->addMinutes(20), function () use ($path) {
+        //     return file_get_contents($path);
+        // });
+
+        // of all the blog posts, find the one with a slug that matches the one that was requested.
+        return static::all()->firstWhere('slug', $slug);
     }
 }
