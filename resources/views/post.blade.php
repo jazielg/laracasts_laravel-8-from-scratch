10f7e737e0fb6648e8c94c81f81b1f371388eee0
jazielg <jazielgj1995@gmail.com>
2021-09-07 13:40:25 -0300

    8 - store blog posts as html files

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,11 +5,7 @@
 
 <body>
     <article>
-        <h1 href="/post">My First Post</h1>
-
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In quo labore quisquam eligendi enim voluptatibus ratione fugit nesciunt unde modi! Dolorum recusandae placeat excepturi, explicabo temporibus quidem quia adipisci quam.
-        </p>
+        <?= $post ?>
     </article>
 
     <a href="/">Go Back</a>
