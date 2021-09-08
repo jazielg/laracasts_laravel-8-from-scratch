8d055e680762598b13178b73ae4b023db212378d
jazielg <jazielgj1995@gmail.com>
2021-09-08 20:43:51 -0300

    25 - show all posts associated with a category

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
@@ -28,3 +29,9 @@ Route::get('posts/{post:slug}', function (Post $post) {
         'post' => $post
     ]);
 }); // ->where('post', '[A-z_\-]+')
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
+    ]);
+});
