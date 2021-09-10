67e62cadc725e64c1e87c070eefb67182435978f
jazielg <jazielgj1995@gmail.com>
2021-09-10 19:31:41 -0300

    34 - a small javascript dropdown detour

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,8 @@ Route::get('/', function () {
     // });
 
     return view('posts', [
-        'posts' => Post::latest('published_at')->with(['category', 'author'])->get()
+        'posts' => Post::latest('published_at')->with(['category', 'author'])->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -35,12 +36,15 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts->load(['category', 'author'])
+        'posts' => $category->posts->load(['category', 'author']),
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts->load(['category', 'author'])
+        'posts' => $author->posts->load(['category', 'author']),
+        'categories' => Category::all()
     ]);
 });
