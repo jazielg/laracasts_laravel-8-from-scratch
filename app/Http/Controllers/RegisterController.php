b3022b92d456cc8923350a98675e9c4748b3f71a
jazielg <jazielgj1995@gmail.com>
2021-09-12 16:28:55 -0300

    49 - login and logout

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -25,7 +25,10 @@ class RegisterController extends Controller
 
         // $attributes['password'] = bcrypt($attributes['password']);
 
-        User::create($attributes);
+        $user = User::create($attributes);
+
+        // log the user in
+        auth()->login($user);
 
         // session()->flash('success', 'Your account has been created.');
         return redirect('/')->with('success', 'Your account has been created.');
