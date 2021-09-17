4a144c90f13823f95c5ff5a63e679da8a14bf616
jazielg <jazielgj1995@gmail.com>
2021-09-16 21:14:16 -0300

    67 - create a form to edit and delete posts

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -3,7 +3,12 @@
 <x-form.field>
     <x-form.label name="{{ $name }}" />
 
-    <input name="{{ $name }}" id="{{ $name }}" class="border border-gray-200 p-2 w-full rounded" value="{{ old($name) }}" required {{ $attributes }}>
+    <input
+        class="border border-gray-200 p-2 w-full rounded"
+        name="{{ $name }}"
+        id="{{ $name }}"
+        {{ $attributes(['value' => old($name)]) }}
+    >
 
     <x-form.error name="{{ $name }}" />
 </x-form.field>
